370da0d081424e7ef168655a53dac5e2daef1524
junho.m <junhot08@gmail.com>
2022-04-22 10:17:17 +0900

    final

---
+++ music.py
@@ -0,0 +1,6 @@
+from playsound import playsound
+
+playsound('img/perfect.mp3')
+playsound('img/edsheeran.mp3')
+playsound('img/ending.mp3')
+playsound('img/perfect.mp3')
--- questions.py
+++ questions.py
@@ -1,4 +1,5 @@
 import cv2
+from playsound import playsound
 
 def img_show(file, dur):
     cv2.imshow(file, cv2.imread(file))
@@ -16,14 +17,14 @@ def q1():
     while ans != '4':
         ans = input('ans: ')
         if ans != '4':
-            print('빵귀 ㅡㅡ')
+            print('뽀귀? 빵귀..')
         else:
             print('뽀귀야? ㅇ3ㅇ 축하해요~~ ㅇ3ㅇ')
             break
 
 def q2():
-    q = 'Q2. 이 사진은 2019년 5월 27일, 어느 공원에서 찍힌 사진입니다. 사진 속 뽀귀는 어두운 시간까지 무엇을 열심히 보고 있었을까요?'
-    a1 = '1) 인스타그램'
+    q = 'Q2. 이 사진은 2019년 5월 27일, 난지의 공원에서 찍힌 사진입니다. 사진 속 뽀귀는 어두운 시간까지 무엇을 열심히 보고 있었을까요?'
+    a1 = '1) 인스타그램 스토리'
     a2 = '2) 베트남 여행 정보'
     a3 = '3) 시그널'
     a4 = '4) WWW'
@@ -35,7 +36,7 @@ def q2():
     while ans != '3':
         ans = input('ans: ')
         if ans != '3':
-            print('빵귀 ㅡㅡ')
+            print('뽀귀? 빵귀..')
         else:
             print('시그널 보다가 어두워졌어 ㅇㅅㅇ')
             break
@@ -56,7 +57,7 @@ def q3():
     while ans != '3':
         ans = input('ans: ')
         if ans != '3':
-            print('빵귀 ㅡㅡ')
+            print('뽀귀? 빵귀..')
         else:
             print('우리 백일~~~')
             break
@@ -69,7 +70,7 @@ def q4():
     a1 = '1) 도림천 산책 하다가 운동하기'
     a2 = '2) 자전거 타고 도림천 달리다가 핑크뮬리 보기'
     a3 = '3) 탄천 드라이브'
-    a4 = '4) 문래동 공원 런닝하다가 병원 가기'
+    a4 = '4) 문래동 공원 런닝하다가 배아파서 병원 가기'
     print(q)
     print('{}\n{}\n{}\n{}'.format(a1, a2, a3, a4))
     ans = 0
@@ -77,7 +78,7 @@ def q4():
     while ans != '2':
         ans = input('ans: ')
         if ans != '2':
-            print('빵귀 ㅡㅡ')
+            print('뽀귀? 빵귀..')
         else:
             print('갑자기 근처에서 핑크뮬리를 만난 날')
             break
@@ -96,10 +97,12 @@ def q5():
     while ans != '3':
         ans = input('ans: ')
         if ans != '3':
-            print('빵귀 ㅡㅡ')
+            print('뽀귀? 빵귀..')
         else:
             print('유람선은 가평이랑 여수~')
-            print('2019년 겨울, 2020년 여름, 2021년 가을')
+            print('2019년 겨울 카멜리아힐에서 우리의 첫 제주')
+            print('2020년 여름 씽씽이 두시간 탔던 날')
+            print('2021년 가을 백신 맞고 몸이 안좋았던 여행')
             break
     img_show('img/q5_1.JPG', 5)
     img_show('img/q5_2.jpg', 5)
@@ -117,21 +120,22 @@ def q6():
     ans = 0
     img_show('img/q6_1.jpg', 5)
     img_show('img/q6_2.jpg', 5)
+    # playsound('img/youtube.mp3')
     while ans != '1':
         ans = input('ans: ')
         if ans != '1':
-            print('빵귀 ㅡㅡ')
+            print('뽀귀? 빵귀..')
         else:
-            print('NPC 없으면 뽀귀 꼴등~~')
+            print('NPC 없으면 꼴등~~')
+            # playsound('img/npc.mp3')
             break
     cv2.destroyAllWindows()
 
 def q7():
-    q = 'Q7. 2021년 7월 31일 한 여름날 사진입니다.\n뽀귀가 공부하는 호구를 위해 음식 선택지를 준비했습니다. \
-        남은 메뉴 중 호구가 가장 먼저 선택한 메뉴는?'
+    q = 'Q7. 2021년 7월 31일 한 여름날 사진입니다. \n뽀귀가 공부하는 호구를 위해 음식 선택지를 준비했습니다. 남은 메뉴 중 호구가 가장 먼저 선택한 메뉴는?'
     a1 = '1) 광화문 미진이'
     a2 = '2) 고귀리 막국수'
-    a3 = '3) 떡만두국~'
+    a3 = '3) 떡만두국'
     a4 = '4) 유부초밥+김치우동'
     print(q)
     print('{}\n{}\n{}\n{}'.format(a1, a2, a3, a4))
@@ -140,9 +144,9 @@ def q7():
     while ans != '2':
         ans = input('ans: ')
         if ans != '2':
-            print('빵귀 ㅡㅡ')
+            print('뽀귀? 빵귀..')
         else:
-            print('다음엔 호랑이 타고 고귀리 직접 가서 먹자')
+            print('앞으로 호랑이 타고 직접 자주 맛집에 가서 맛있는 걸 먹어요')
             break
     img_show('img/q7_2.JPG', 5)
     cv2.destroyAllWindows()
@@ -160,13 +164,34 @@ def q8():
     while ans != '3':
         ans = input('ans: ')
         if ans != '3':
-            print('빵귀 ㅡㅡ')
+            print('뽀귀? 빵귀..')
         else:
             print('뽀귀는 선물 있는 줄도 모르고 새벽까지 술마시고 놀다가 들어갔어..')
             break
     img_show('img/q8_3.jpg', 4)
     img_show('img/q8_2.JPG', 4)
     cv2.destroyAllWindows()
+
+def q9():
+    q = 'Q9. 테이블 위의 떡만두국은 이런 날이 오면 내가 꼭 준비해주고 싶던 음식입니다.\n뽀귀를 결혼하고 싶은 귀신이라고 느꼈던 일이라서요.\n그때 만두국을 먹고 갔던 산 이름이 무엇일까요?'
+    a1 = '1) 관악산'
+    a2 = '2) 인왕산'
+    a3 = '3) 안산'
+    a4 = '4) 청계산'
+    print(q)
+    print('{}\n{}\n{}\n{}'.format(a1, a2, a3, a4))
+    ans = 0
+    while ans != '4':
+        ans = input('ans: ')
+        if ans != '4':
+            print('뽀귀? 빵귀..')
+        else:
+            print('2020년 4월 12일 그날에 감동 받았던 떡만두국이야.')
+            img_show('img/q9_2.jpg', 5)
+            img_show('img/q9_1.jpg', 5)
+            cv2.destroyAllWindows()
+            break
+    
 
 if __name__ == '__main__':
     pass
